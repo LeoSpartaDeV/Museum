cec74f109c8763765029c439ece29ef62ca75ccd
Admin <lenovichiv@gmail.com>
2025-07-30 00:50:41 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,55 @@
 'use strict';
+import Swiper from 'swiper';
+import { Pagination } from 'swiper/modules';
+
+import 'swiper/swiper-bundle.css';
+
+let swiperInstance = null;
+
+function initSwiper() {
+  const screenWidth = window.innerWidth;
+
+  if (screenWidth >= 1280) {
+    if (swiperInstance) {
+      swiperInstance.destroy(true, true);
+      swiperInstance = null;
+    }
+
+    return;
+  }
+
+  if (!swiperInstance) {
+    swiperInstance = new Swiper('.gallery__slider', {
+      modules: [Pagination],
+      slidesPerView: 1,
+      spaceBetween: 20,
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+      },
+      breakpoints: {
+        768: {
+          slidesPerView: 2,
+        },
+      },
+    });
+  }
+}
+
+function throttle(func, delay) {
+  let lastCall = 0;
+
+  return function(...args) {
+    const now = new Date().getTime();
+
+    if (now - lastCall >= delay) {
+      lastCall = now;
+      func(...args);
+    }
+  };
+}
+
+const throttledSwiper = throttle(initSwiper, 200);
+
+window.addEventListener('load', initSwiper);
+window.addEventListener('resize', throttledSwiper);
